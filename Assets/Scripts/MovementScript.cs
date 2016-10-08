f8af46d08e2ac32ceddfaad24a8a6e2808e20f50
Jeff Robbins <RobbinsJeffreyM@gmail.com>
2016-10-07 21:41:16 -0400

    Basic movement

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+
+public class MovementScript : MonoBehaviour
+{
+	public int playerID;
+	public float moveSpeed;
+	Player player;
+	Rigidbody rigid;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = ReInput.players.GetPlayer(playerID);
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		rigid.AddForce( Vector3.forward * ( moveSpeed * player.GetAxis("vert axis")));
+		rigid.AddForce(Vector3.right * (moveSpeed * player.GetAxis("horiz axis")));
+	}
+}

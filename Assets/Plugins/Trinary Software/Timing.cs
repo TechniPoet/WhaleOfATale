3d59a96196840f47bd7698ed9db01fc6c1f2f7f5
Jeff Robbins <RobbinsJeffreyM@gmail.com>
2016-10-07 20:56:11 -0400

    fixed issue

--- Assets/Plugins/Trinary Software/Timing.cs
+++ Assets/Plugins/Trinary Software/Timing.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.Profiling;
 
 // /////////////////////////////////////////////////////////////////////////////////////////
 //                                        TIMING

b59ec411da28e69de4773295ff1d238b2aeb32b2
Jeff Robbins <RobbinsJeffreyM@gmail.com>
2016-10-09 09:43:31 -0400

    Added ghosts

---
+++ Assets/Scripts/GhostModelRand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostModelRand : MonoBehaviour {
+	public GameObject face1;
+	public GameObject face2;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (Random.Range(0, 1f) > 0.5f)
+		{
+			face1.SetActive(false);
+		}
+		else
+		{
+			face2.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
